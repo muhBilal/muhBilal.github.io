14025ea952cf053a9bc15c07d57b7bf3b8aca0a8
muhBilal <mb336450@gmail.com>
2022-04-16 21:25:06 +0700

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors:{
+        'body' : '#17171f',
+        'selected-text': '#A3A3ff',
+        'theme' : '#3f3fff',
+        'nav' : '#404053',
+        'secondary' : '#9191a4',
+        'badge': '#3f3f51',
+        'input-border' : '#565666',
+        'input' : '#2a2a35'
+      },
+      fotfamily:{
+        'poppins' : ["'Poppins'", 'sans-serif']
+      }
+    },
+  },  
+  plugins: [],
+}
